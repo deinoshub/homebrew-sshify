59368d7de4e3ee5201b3bc8adce19a925cedef9d
Lê Thu Hà <122956583+deinoshub@users.noreply.github.com>
2026-08-24 22:42:56 +0700

    Add sshify 0.1.0 formula

---
+++ Formula/sshify.rb
@@ -0,0 +1,18 @@
+class Sshify < Formula
+  desc "TUI to manage SSH hosts for the Bitwarden SSH agent"
+  homepage "https://github.com/deinoshub/sshify"
+  url "https://github.com/deinoshub/sshify/archive/refs/tags/v0.1.0.tar.gz"
+  sha256 "9d619e75e979d9c6066cdae05e247289180bc0833c5fdbf43d5ba8778ed8c530"
+  head "https://github.com/deinoshub/sshify.git", branch: "main"
+
+  depends_on "go" => :build
+
+  def install
+    ldflags = "-s -w -X main.version=#{version}"
+    system "go", "build", *std_go_args(ldflags:), "./cmd/sshify"
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/sshify -v")
+  end
+end
---
+++ README.md
@@ -0,0 +1,14 @@
+# homebrew-sshify
+
+Homebrew tap for [sshify](https://github.com/deinoshub/sshify).
+
+```bash
+brew install deinoshub/sshify/sshify
+```
+
+Upgrade:
+
+```bash
+brew update
+brew upgrade sshify
+```
