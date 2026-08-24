8340682dc0af61e3a50f8a07b409659a896aec6a
Lê Thu Hà <122956583+deinoshub@users.noreply.github.com>
2026-08-24 22:43:44 +0700

    Avoid duplicating -s -w ldflags already added by std_go_args

--- Formula/sshify.rb
+++ Formula/sshify.rb
@@ -8,7 +8,7 @@ class Sshify < Formula
   depends_on "go" => :build
 
   def install
-    ldflags = "-s -w -X main.version=#{version}"
+    ldflags = "-X main.version=#{version}"
     system "go", "build", *std_go_args(ldflags:), "./cmd/sshify"
   end
 
